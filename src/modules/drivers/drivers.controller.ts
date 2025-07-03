41d585829fdd30ab4acb275a831d1a1b6ce6721d
HuynhSang2005 <huynhsang060305@gmail.com>
2025-07-03 21:31:26 +0700

    feat(drivers): implement endpoints to create and list drivers

--- src/modules/drivers/drivers.controller.ts
+++ src/modules/drivers/drivers.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { DriversService } from './drivers.service';
+import { CreateDriverDto } from './dto/create-driver.dto';
 
 @Controller('drivers')
 export class DriversController {
   constructor(private readonly driversService: DriversService) {}
+
+  @Post()
+  create(@Body() createDriverDto: CreateDriverDto) {
+    return this.driversService.create(createDriverDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.driversService.findAll();
+  }
 }
--- src/modules/drivers/drivers.service.ts
+++ src/modules/drivers/drivers.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateDriverDto } from './dto/create-driver.dto';
+import { PrismaService } from 'core/prisma/prisma.service';
 
 @Injectable()
-export class DriversService {}
+export class DriversService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createDriverDto: CreateDriverDto) {
+    return this.prisma.driver.create({ data: createDriverDto });
+  }
+
+  findAll() {
+    return this.prisma.driver.findMany();
+  }
+}
