90bf2a40359bda9b91cb3e45a091d00c9fe7ae56
HuynhSang2005 <huynhsang060305@gmail.com>
2025-07-03 13:55:20 +0700

    chore: integrate bookings module into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaModule } from 'core/prisma/prisma.module';
 import { OsrmModule } from 'core/orsm/osrm.module';
+import { BookingsController } from './modules/bookings/bookings.controller';
 
 @Module({
-  imports: [PrismaModule, OsrmModule],
+  imports: [PrismaModule, OsrmModule, BookingsController],
   controllers: [AppController],
   providers: [],
 })
