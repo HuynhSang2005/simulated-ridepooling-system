a0ff2d68df025ce70d4d8412c89200cdc7883531
HuynhSang2005 <huynhsang060305@gmail.com>
2025-07-03 14:46:35 +0700

    feat(optimization): fetch pending bookings in cron job

--- src/modules/route-optimization/route-optimization.service.ts
+++ src/modules/route-optimization/route-optimization.service.ts
@@ -1,13 +1,32 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { BookingStatus } from '@prisma/client';
+import { PrismaService } from 'core/prisma/prisma.service';
 
 @Injectable()
 export class RouteOptimizationService {
   private readonly logger = new Logger(RouteOptimizationService.name);
 
-  // sẽ auto run mỗi 30 giây
-  @Cron(CronExpression.EVERY_5_SECONDS)
-  handleCron() {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  async handleCron() {
     this.logger.debug('Running route optimization job...');
+
+    // 1. Lấy tất cả các booking đang chờ
+    const pendingBookings = await this.prisma.booking.findMany({
+      where: {
+        status: BookingStatus.PENDING,
+      },
+    });
+
+    // 2. Nếu không có đủ booking, bỏ qua lần chạy này
+    //  Đ0ặt ngưỡng tối thiểu, hiện tại thì sẽ ví dụ là 2 booking
+    if (pendingBookings.length < 2) {
+      this.logger.debug('Not enough pending bookings to create a route. Skipping.');
+      return;
+    }
+
+    this.logger.log(`Found ${pendingBookings.length} pending bookings to process.`);
   }
 }
